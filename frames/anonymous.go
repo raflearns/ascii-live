3193aad21ad85128f9678c6dc665a9ddca61da8c
raflearns <147781291+raflearns@users.noreply.github.com>
2026-03-13 23:59:48 +0800

    Create anonymous.go
    Anonymous Logo

---
+++ frames/anonymous.go
@@ -0,0 +1,30 @@
+package frames
+
+// Anonymous is the exported variable used in frames.go
+var Anonymous = DefaultFrameType(anonymousFrames)
+
+var anonymousFrames = []string{
+	`
+     .edee...      
+   .E"  eeeeee.    
+  .E"  .E"  "E.    
+ .E"   .E"   "E.   
+.E"    .E"    "E.  
+      .EEEEEEE.    
+      .E"   "E.    
+      .E"   "E.    
+       "eeeee"     
+	`,
+	`
+     .edee...      
+   .E"  eeeeee.    
+  .E"  .E"  "E.    
+ .E"   .E"   "E.   
+.E"    .E"    "E.  
+      .EEEEEEE.    
+      .E"   "E.    
+      .E"   "E.    
+       "eeeee"     
+        "eee"      
+	`,
+}
